8c9c8b8b77e437d099267cda5a149ef8741ff653
akash <4k45hr0ck5007@gmail.com>
2025-12-15 13:15:08 +0530

    nit

--- config/settings/base.py
+++ config/settings/base.py
@@ -162,9 +162,9 @@ MIDDLEWARE = [
 # STATIC
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(ROOT_DIR.path("staticfiles"))
-# https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = "/static/"
+STATIC_ROOT = "/var/www/docatho/static"
+
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [str(APPS_DIR.path("static"))]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
